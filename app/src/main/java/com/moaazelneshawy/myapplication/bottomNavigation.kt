8a2e39ffea5c331d2c7ca6bc6526ada3e3e08f0a
MoaazElneshawy <m.elneshawy@gmail.com>
2023-07-04 06:44:55 +0300

    use label property instead

--- app/src/main/java/com/moaazelneshawy/myapplication/bottomNavigation.kt
+++ app/src/main/java/com/moaazelneshawy/myapplication/bottomNavigation.kt
@@ -1,7 +1,10 @@
 package com.moaazelneshawy.myapplication
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -10,13 +13,10 @@ import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -116,33 +116,29 @@ fun BottomNavigationBar(
             BottomNavigationItem(
                 selected = selected, onClick = { onItemClicked(item) },
                 icon = {
-                    Column(horizontalAlignment = CenterHorizontally) {
-                        if (item.badgeCount > 0) {
-                            BadgedBox(
-                                badge = {
-                                    Text(
-                                        text = "${item.badgeCount}",
-                                        modifier = Modifier
-                                            .background(
-                                                Color.Red,
-                                                shape = CircleShape
-                                            )
-                                            .padding(1.dp)
-                                    )
-                                }
-                            ) {
-                                Icon(imageVector = item.image, contentDescription = item.title)
+                    if (item.badgeCount > 0) {
+                        BadgedBox(
+                            badge = {
+                                Text(
+                                    text = "${item.badgeCount}",
+                                    modifier = Modifier
+                                        .background(
+                                            Color.Red,
+                                            shape = CircleShape
+                                        )
+                                        .padding(1.dp)
+                                )
                             }
-                        } else {
+                        ) {
                             Icon(imageVector = item.image, contentDescription = item.title)
                         }
-                        if (selected) Text(
-                            text = item.title, color = Color.White,
-                            fontWeight = FontWeight.Bold,
-                            fontSize = 10.sp
-                        )
+                    } else {
+                        Icon(imageVector = item.image, contentDescription = item.title)
                     }
+
                 },
+                alwaysShowLabel = false,
+                label = { Text(text = item.title) },
                 selectedContentColor = Color.White,
                 unselectedContentColor = Color.LightGray
             )
